9b1d1d6f4618cd93ad89712f92065bcdbf2e1469
Todd Christensen <toddchristensen@Todds-MacBook-Air.local>
2021-06-23 08:23:13 +0800

    adding fleek.io to the footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -15,7 +15,7 @@ return (
       } flex items-center justify-center flex-col`}
     >
       <div className="container px-4 p-4 flex flex-row flex-wrap justify-center items-center mb-16 lg:mb-0">
-        Site created by me, Todd Christensen.
+        This site is hosted on Internet Computer (Decentralized Web) via <a rel="noopener noreferrer" target="_blank" href="https://fleek.io">Fleek.io</a>
       </div>
     </section>
   </>
